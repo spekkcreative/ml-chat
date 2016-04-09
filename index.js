cce8b50a383e9f3d8cac9e3390bb81c52dbddfbf
Brad Filip <brad@bradfilip.com>
2016-04-08 22:30:39 -0700

    test user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var user = "billythefridge";
+//var user = "billythefridge";
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <style>
       * { margin: 0; padding: 0; box-sizing: border-box; }
       body { font: 13px Helvetica, Arial; }
-      form { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
-      form input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }
+      form { background: #ccc; padding: 3px; position: fixed; bottom: 0; width: 100%; }
+      form input { border: 0; padding: 10px; width: 80%; margin-right: .5%; }
       form button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
       #messages { list-style-type: none; margin: 0; padding: 0; }
       #messages li { padding: 5px 10px; }
@@ -21,6 +21,7 @@
     <script src="/socket.io/socket.io.js"></script>
     <script src="http://code.jquery.com/jquery-1.11.1.js"></script>
     <script>
+    var user = <?php $_GET['user']; ?>;
       var socket = io();
       $('form').submit(function(){
         socket.emit('chat message', $('#m').val());
